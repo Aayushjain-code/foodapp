3c178ecf1dcfd7d9644bc83af767c79b9e2334e7
Aayus Jain <aayushjain6020@gmail.com>
2022-01-13 18:24:22 +0530

    Basic functionality of app completed with axios

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,38 +3,42 @@ import { StyleSheet, Text, TextInput, View } from 'react-native'
 import Feather from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
-const SearchBar = () => {
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
     return (
         <View style={styles.backgroundStyle}>
             <Feather name="magnify" style={styles.iconStyle} />
             <TextInput
-                autoCapitalize="none"
-                autoCorrect={false}
-                style={styles.inputStyle}
-                placeholder="Search"
-            />
-        </View>
-    )
-}
-
-export default SearchBar
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    backgroundStyle: {
-        marginTop: 10,
-        backgroundColor: '#F0EEEE',
-        height: 50,
-        borderRadius: 5,
-        marginHorizontal: 15,
-        flexDirection: 'row'
-      },
-      inputStyle: {
-        flex: 1,
-        fontSize: 18
-      },
-      iconStyle: {
-        fontSize: 35,
-        alignSelf: 'center',
-        marginHorizontal: 15
-      }
-})
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
+  }
+});
+
+export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,37 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import SearchBar from '../components/SearchBar'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
-const SearchScreen = ( {navigation }) => {
-    return (
-        <View style={styles.body}>
-            <SearchBar/>
-        </View>
-    )
-}
+const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-export default SearchScreen
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
 
-const styles = StyleSheet.create({
-    body: {
-        
-    },
-})
+  return (
+    <View>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
