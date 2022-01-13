33618ea69e9e5ea13790457fbe21407695648d57
Aayus Jain <aayushjain6020@gmail.com>
2022-01-13 15:07:26 +0530

    first commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SearchScreen = ( {navigation }) => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>search</Text>
+        </View>
+    )
+}
+
+export default SearchScreen
+
+const styles = StyleSheet.create({})
